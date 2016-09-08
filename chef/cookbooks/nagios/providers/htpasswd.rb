308dd6f9a082bc557d36b2e93fe60dbe2d8bfa7d
Farah Schüller <fschueller@suse.de>
2016-09-08 17:44:06 +0200

    Add provider and delete action for htpasswd file

---
+++ chef/cookbooks/nagios/providers/htpasswd.rb
@@ -0,0 +1,32 @@
+#
+# Author:: Farah Schüller <fschueller@suse.com>
+# Author:: Felix Schnizlein <fschnizlein@suse.com>
+# Cookbook Name:: nagios
+# Provider:: htpasswd
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+action :delete do
+  file @new_resource.file do
+    action :delete
+    only_if { File.exist?(@new_resource.file)}
+  end
+end
+
+action :add do
+  file @new_resource.file do
+
+  end
+end
